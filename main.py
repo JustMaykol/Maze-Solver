532aed717b1e97e45a9f1b7bcf530429c09723e3
zSpectrx <maykol2m2002@gmail.com>
2020-11-26 11:05:51 -0500

    .

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import util.position
+
 WALL = '#'
 BLANK = ' '
 
@@ -9,13 +11,3 @@ maze = [['#', '#', '#', '#', '#'],
         ['#', ' ', ' ', ' ', '#'],
         ['#', ' ', ' ', ' ', '#'],
         ['#', '#', '#', '#', '#']]
-
-
-def printMaze(value):
-    for line in value:
-        for key in line:
-            print(key, end=' ')
-        print(' ')
-
-
-print(getNear([2, 2]))
--- util/position.py
+++ util/position.py
@@ -1,19 +1,19 @@
 # Up, Right, Down, Left
-def getNear(x, y):
-    return [getUp(x, y), getRight(x, y), getDown(x, y), ]
+def get_near(x, y):
+    return [get_up(x, y), get_right(x, y), get_down(x, y), get_left(x, y)]
 
 
-def getUp(x, y):
+def get_up(x, y):
     return [x, y - 1]
 
 
-def getRight(x, y):
+def get_right(x, y):
     return [x + 1, y]
 
 
-def getDown(x, y):
+def get_down(x, y):
     return [x, y + 1]
 
 
-def getLeft(x, y):
+def get_left(x, y):
     return [x - 1, y]
